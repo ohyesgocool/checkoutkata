37128a7ebb1381e9f032584b8e08048e12f9bdca
Gokul Menon <65238256+ohyesgocool@users.noreply.github.com>
2025-06-13 17:09:53 +0200

    init repo

---
+++ src/main/java/org/example/checkoutkata/CheckoutkataApplication.java
@@ -0,0 +1,13 @@
+package org.example.checkoutkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutkataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutkataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/checkoutkata/CheckoutkataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.checkoutkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutkataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
