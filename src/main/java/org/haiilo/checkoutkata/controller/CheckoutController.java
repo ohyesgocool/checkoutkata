ebe50b81e21358b09388c9be4063c4a1d524cf35
Gokul Menon <65238256+ohyesgocool@users.noreply.github.com>
2025-06-16 00:36:33 +0200

    remove wildcard imports

--- src/main/java/org/haiilo/checkoutkata/controller/CheckoutController.java
+++ src/main/java/org/haiilo/checkoutkata/controller/CheckoutController.java
@@ -5,7 +5,10 @@ import org.haiilo.checkoutkata.dto.CheckoutRequest;
 import org.haiilo.checkoutkata.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("v1/checkout")
