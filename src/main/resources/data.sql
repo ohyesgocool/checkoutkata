e810838ce4053d47173046bbda8e799e6426b91a
Gokul Menon <65238256+ohyesgocool@users.noreply.github.com>
2025-06-15 16:09:14 +0200

    fix h2 database issue

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,12 +24,8 @@ INSERT INTO item (name, unit_price) VALUES ('Mango', 80);
 INSERT INTO item (name, unit_price) VALUES ('Grapes', 35);
 
 -- Seed Offers
-INSERT INTO offer (item_name, quantity, offer_price) VALUES ('apple', 2, 45);         -- 2 for 45 instead of 60
-INSERT INTO offer (item_name, quantity, offer_price) VALUES ('banana', 3, 130);       -- 3 for 130 instead of 150
-INSERT INTO offer (item_name, quantity, offer_price) VALUES ('Peach', 2, 100);        -- 2 for 100 instead of 120
-INSERT INTO offer (item_name, quantity, offer_price) VALUES ('Kiwi', 5, 90);          -- 5 for 90 instead of 100
-INSERT INTO offer (item_name, quantity, offer_price) VALUES ('Grapes', 4, 120);       -- 4 for 120 instead of 140
-
-
-SELECT * FROM item;
-SELECT * FROM offer;
+INSERT INTO offer (item_name, quantity, offer_price) VALUES ('apple', 2, 45);
+INSERT INTO offer (item_name, quantity, offer_price) VALUES ('banana', 3, 130);
+INSERT INTO offer (item_name, quantity, offer_price) VALUES ('Peach', 2, 100);
+INSERT INTO offer (item_name, quantity, offer_price) VALUES ('Kiwi', 5, 90);
+INSERT INTO offer (item_name, quantity, offer_price) VALUES ('Grapes', 4, 120);
